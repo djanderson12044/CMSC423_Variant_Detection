f46b5489914d571a59bdbfcd0d1353ba91009c7e
deejandy <djanderson12044@gmail.com>
2020-04-10 21:37:52 -0400

    home stretch

--- fmmap.py
+++ fmmap.py
@@ -106,7 +106,7 @@ def index(ref, ref_index):
         pickle.dump(return_index, outfile)
         outfile.close()
 
-    print(record.seq)
+
 
 
 # ref_index: (.?) File from the index definition
@@ -117,7 +117,11 @@ def align(ref_index, reads, aligns):  # todo Finish implementing align method
     ninf = float("-inf")
     #seed_skip = lambda l: math.floor(l / 5.0) # TODO Real Seed Skip Function
     seed_skip = lambda l: math.floor(l / 2.0)
-    gap = 5
+    gap = -2 # I think must be the same as the insertion/deletion cost that we have
+
+    r_index = {}
+    with open(ref_index + ".P", "r+b") as infile:
+        r_index = pickle.load(infile)
 
     # with gzip.open(reads + '.fa.gz', 'rt') as rfile: # TODO Real file is gziped
     for read in SeqIO.parse(reads, "fasta"):
@@ -126,42 +130,26 @@ def align(ref_index, reads, aligns):  # todo Finish implementing align method
         best_score = ninf
         seed_pos = 0
         skip = seed_skip(read_len)
-        for seed_start in range(0, read_len, skip):
-            seed_end = min(read_len, seed_start + skip)
+        q_len = len(read.seq)
+
+        for seed_start in range(0, read_len, skip): # TODO need to put this back
+            seed_end = min(read_len, seed_start + skip) # offset of the read where the seed ends.
+
             interval, match_len = get_interval(read.seq[seed_start:seed_end], ref_index)
 
-
-        # given all the places where the seed matches, look for an alignment around it
-        # the ref_positions member of `bwt_index` will return positions on the reference
-        # string corresponding to the *beginning of the read*, assuming there are no gaps
-        # in the alignment before the start of the seed (handling that is why we do fitting
-        # alignment below).
-        for ref_pos in ref_positions(interval, seed_end, match_len):
-            pass
-
-        '''
-            # Perform a "fitting" alignment of the query (seq) into the reference (ref)
-            # the returned alignment object contains the score, the alignment and the
-            # implied position where the query (seq) begins under the alignment.
-            # To perform the fitting alignment, you should "slice out" a region of the
-            # reference around the implied start position (ref_pos) with a bit of padding
-            # (e.g. gap bases) before the first base where the read would start and after
-            # the last base where the read would end.  This will ensure that the fitting_alignment
-            # procedure can find the optimal position for the query within the reference
-            # window that contains it, even if there are insertions or deletions in the read.
-            alignment = fitting_alignment(seq, ref, ref_pos, gap)
-            if alignment.score > best_score:
-                best_score = alignment.score
-                alignments = [alignment]
-            elif alignment.score == best_score:
-                alignments.append(alignment)
-        for a in alignments:
-            write_to_sam(output_file, a)
-        '''
+            for ref_pos in ref_positions(interval, seed_end, match_len, q_len, r_index["sa"]):
+                alignment = fitting_alignment(read.seq, r_index["ref"], ref_pos, gap)
+                if alignment["score"] > best_score: # TODO Need to implement this. Also think about bound exceptions
+                    best_score = alignment["score"]
+                    alignments = [alignment]
+                elif alignment["score"] == best_score:
+                    alignments.append(alignment)
+            for a in alignments:
+                print(a)
+            #    write_to_sam(output_file, a)
 
 
 # Align Helper Methods-----------------------------------------
-
 def get_interval(pattern, ref_index):
     r_index = {}
     with open(ref_index + ".P", "r+b") as infile:
@@ -170,23 +158,115 @@ def get_interval(pattern, ref_index):
     # x is start of interval, y is end of interval
     x, y = bbwm(r_index["occ"], r_index["lColumn"], pattern)
 
-    print(pattern)
-    print(x, y)
-    print("--------------------------")
-
-    for n in range(x, y + 1):
-        print(n, r_index["ref"][r_index["sa"][n]:])
-
-    print()
-
     if x < 0 or y < 0:
         return (-1, -1), -1
     else:
         return (x, y), len(pattern)
 
 
-def ref_positions(interval, seed_end, match_len): # TODO Need to implement
-    return 1, 2
+# interval: interval of sa indexes that match exactly with the pattern
+# sed_end: offset of the read where the seed ended
+# match_len: length of the match
+def ref_positions(interval, seed_end, match_len, q_len, sa): # TODO Need to implement
+    positions = []
+
+    if match_len > 0:
+        for p in range(interval[0], interval[1] + 1):
+            temp = sa[p] - (seed_end - match_len)
+            if temp - 5 >= 0 and temp + q_len + 5 < len(sa) - 1:
+                positions.append(temp)
+
+    return positions
+
+
+# The returned alignment object contains the score, the alignment and the
+#  implied position where the query (seq) begins under the alignment.
+def fitting_alignment(seq, ref, ref_pos, gap):
+    ref_slice = ref[ref_pos - 5: ref_pos + len(seq) + 5]
+    match = 0
+    mismatch = -2
+    insertion = gap
+    deletion = gap
+
+    result = {}
+
+    opt = [[]]
+    # -5: terminal
+    # 1: left
+    # 0: diagonal
+    # -1: down
+    d_opt = [[]]  # Stores direction of the opt
+
+    max_score = float("-inf")
+    max_opt = (-1, -1)
+
+    # Building opt table ------------------------------
+    for x in range(0, len(ref_slice) + 1):
+        opt[0].append(0)
+        d_opt[0].append(-5)
+
+    for j in range(1, len(seq) + 1):
+        opt.append([j * gap])
+        d_opt.append([-1])
+        for i in range(1, len(ref_slice) + 1):
+            diag = (match if ref_slice[i - 1] == seq[j - 1] else mismatch) + opt[j - 1][i - 1]
+            try:
+                left = insertion + opt[j][i - 1]
+            except IndexError:
+                print("Here is the left error")
+            try:
+                down = deletion + opt[j - 1][i]
+            except IndexError:
+                print("Here is the down error")
+
+            temp = max(diag, left, down)
+
+            # Initializing d_opt table
+            if diag == temp:
+                d_opt[j].append(0)
+            elif left == temp:
+                d_opt[j].append(1)
+            elif down == temp:
+                d_opt[j].append(-1)
+            else:
+                print("OH FRICK, something went wrong")
+
+            if j == len(seq):
+                if temp >= max_score:
+                    max_score = temp
+                    max_opt = (j, i)
+
+            opt[j].append(temp)
+    # Finish opt table ---------------------------
+
+    result["score"] = max_score
+
+    # TODO Need to figure out cigar string
+    blah = []
+    cigar = []
+    prev = ""
+    while max_opt[0] > 0:
+        if d_opt[max_opt[0]][max_opt[1]] == 1:
+            max_opt = (max_opt[0], max_opt[1] - 1)
+            blah.insert(0, 'D')
+        elif d_opt[max_opt[0]][max_opt[1]] == 0:
+            max_opt = (max_opt[0] - 1, max_opt[1] - 1)
+            blah.insert(0, 'M')
+        elif d_opt[max_opt[0]][max_opt[1]] == -1:
+            max_opt = (max_opt[0] - 1, max_opt[1])
+            blah.insert(0, 'I')
+        else:
+            print("ruh Roh")
+
+    cigar = []
+    result["offset"] = max_opt[1]
+    result["cigar"] = "".join(blah)
+
+    return result
+
+
+
+
 
 
 # Helper Methods----------------------------------------
@@ -225,9 +305,8 @@ def bbwm(occ, lc, p):
 
     # Initializes the count table, which we can query to get the number of characters
     #  appearing before the character set that we want (in the first column)
-    curr = 1;
-    count_table = {}
-    count_table['$'] = 0  # Not stored in occ so we have to add it
+    curr = 1
+    count_table = {'$': 0}
     for x in sorted(occ.keys()):
         count_table[x] = curr
         curr += occ[x][-1]
@@ -244,6 +323,8 @@ def bbwm(occ, lc, p):
         else:
             return top, bottom
     return -1, -1
+
+#cost()
 
 
 # Calls the main() definition
